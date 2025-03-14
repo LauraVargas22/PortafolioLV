a8f54fcf2c7c21352b021edd6f9b2987928c4fd5
Laura <181015028+LauraVargas22@users.noreply.github.com.>
2025-03-14 17:31:01 -0500

    feat: :sparkles: Improve the project structure

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './css/styles.css';
+
+const bars = document.querySelector(".bars");
+const nav = document.querySelector("nav");
+
+bars.onclick = () => {
+  nav.classList.toggle("active");
+};
