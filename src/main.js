50c7fcc0139568a0b0848c8ac69d000ef679bb2b
Laura <181015028+LauraVargas22@users.noreply.github.com.>
2025-05-12 19:22:26 -0500

    feat: :sparkles: Languages

--- src/main.js
+++ src/main.js
@@ -5,3 +5,23 @@ const nav = document.querySelector("nav");
 bars.addEventListener("click", () => {
   nav.classList.toggle("active");
 });
+
+
+//Language
+const langButtons = document.querySelectorAll("[data-language]");
+const textToChange = document.querySelectorAll("[data-section]");
+
+langButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    fetch(`/languages/${button.dataset.language}.json`)
+      .then(res => res.json())
+      .then(data => {
+        textToChange.forEach((e) => {
+          const section = e.dataset.section;
+          const value = e.dataset.value;
+
+          e.innerHTML = data[section][value];
+        })
+      })
+  });
+});
